9c79ff57abf21d88c8f3aa0fda0b32b64dc29d4d
Matt Johnson <majikjohnson@hotmail.com>
2019-10-31 13:05:30 +0000

    Added tests for GiphyItem Component

--- src/components/giphy/GiphyItem.js
+++ src/components/giphy/GiphyItem.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-const GiphyItem = ({ url, title, preview }) => {
+const GiphyItem = ({ title, preview, url }) => {
 	return (
 		<div className="col s12">
 			<div className="card-panel blue-grey darken-3">
 				<div className="card-image">
 					<video
+						data-testid="giphy-video-player"
 						className="responsive-video center-align"
 						muted
 						autoPlay
 						loop
 						width="100%"
 					>
-						<source src={preview} type="video/mp4" />
+						<source data-testid="giphy-preview-url" src={preview} type="video/mp4" />
 					</video>
 					<h6>
-						<span className="card-title white-text">{title}</span>
+						<span data-testid="giphy-title" className="card-title white-text">{title}</span>
 					</h6>
 				</div>
 			</div>
---
+++ src/components/giphy/GiphyItem.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import GiphyItem from './GiphyItem';
+
+describe('GiphyItem Component', () => {
+	it('displays the Giphy title correctly', () => {
+		const { getByTestId } = render(
+			<GiphyItem
+				title="Giphy title"
+				preview="https://nacentpixels.io/preview"
+			/>
+		);
+		expect(getByTestId('giphy-title')).toHaveTextContent('Giphy title');
+  });
+
+  it('automatically plays and loops the giphy video', () => {
+		const { getByTestId } = render(
+			<GiphyItem />
+		);
+    
+    const videoPlayer = getByTestId('giphy-video-player');
+    expect(videoPlayer.loop).toBe(true);
+    expect(videoPlayer.muted).toBe(true);
+    expect(videoPlayer.autoplay).toBe(true);
+
+	});
+
+  
+  it('displays the correct Giphy video', () => {
+		const { getByTestId } = render(
+			<GiphyItem
+				title="Giphy title"
+				preview="https://nacentpixels.io/preview"
+			/>
+		);
+		expect(getByTestId('giphy-preview-url')).toHaveAttribute('src', 'https://nacentpixels.io/preview');
+	});
+});
